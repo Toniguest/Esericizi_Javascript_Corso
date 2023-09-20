2bc8ba15a5148ed171086b7c9f3c8c368f6e22d6
Antonio <antonio.pirone@alten.it>
2023-09-20 15:18:01 +0200

    prove esercizi del corso, giusto qualche prova

---
+++ Prove_locali/Esercizio 2/js/script.js
@@ -0,0 +1,14 @@
+let citta;
+
+let nome = 'Antonio'; /* variabile di tipo string */
+
+let eta = 23; /* variabile di tipo number */
+
+let studia = true; /* variabile di tipo booleana [True or False] */
+
+let a = 3;
+let b = 5;
+
+let c = a + b;
+
+console.log(c);
